455e7d258406efb42f648152a7a94ecfb5116f49
Nibesh Napit <nibeshnapit1@gmail.com>
2024-09-22 14:18:59 +0545

    feat: post seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+
         $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
     }
 }
